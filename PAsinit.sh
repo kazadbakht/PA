0a398d064f46f32f1eeba5039437e3517613fbc7
new account <azadbakh@heatman.sen.cwi.nl>
2015-04-09 16:01:30 +0200

    adding the parallel PA codes

---
+++ PAsinit.sh
@@ -0,0 +1,5 @@
+time ./PAinit +RTS -N1 
+time ./PAinit +RTS -N2
+time ./PAinit +RTS -N4
+time ./PAinit +RTS -N8
+time ./PAinit +RTS -N16
---
+++ Run.sh
@@ -0,0 +1,48 @@
+./PAs.sh 2> PAsresults105
+./PAsinit.sh 2> PAsinitresults105
+
+./PAs.sh 2>> PAsresults105
+./PAsinit.sh 2>> PAsinitresults105
+
+./PAs.sh 2>> PAsresults105
+./PAsinit.sh 2>> PAsinitresults105
+
+./PAs.sh 2>> PAsresults105
+./PAsinit.sh 2>> PAsinitresults105
+
+./PAs.sh 2>> PAsresults105
+./PAsinit.sh 2>> PAsinitresults105
+
+./PAs.sh 2>> PAsresults105
+./PAsinit.sh 2>> PAsinitresults105
+
+./PAs.sh 2>> PAsresults105
+./PAsinit.sh 2>> PAsinitresults105
+
+./PAs.sh 2>> PAsresults105
+./PAsinit.sh 2>> PAsinitresults105
+
+./PAs.sh 2>> PAsresults105
+./PAsinit.sh 2>> PAsinitresults105
+
+./PAs.sh 2>> PAsresults105
+./PAsinit.sh 2>> PAsinitresults105
+
+./PAs.sh 2>> PAsresults105
+./PAsinit.sh 2>> PAsinitresults105
+
+./PAs.sh 2>> PAsresults105
+./PAsinit.sh 2>> PAsinitresults105
+
+./PAs.sh 2>> PAsresults105
+./PAsinit.sh 2>> PAsinitresults105
+
+./PAs.sh 2>> PAsresults105
+./PAsinit.sh 2>> PAsinitresults105
+
+./PAs.sh 2>> PAsresults105
+./PAsinit.sh 2>> PAsinitresults105
+
+
+
+
