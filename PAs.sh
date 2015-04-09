30c983947cbb07c5ed0a8d32621129937745bba8
new account <azadbakh@heatman.sen.cwi.nl>
2015-04-09 16:12:27 +0200

    Just reorganizing

--- PAs.sh
+++
@@ -1,5 +0,0 @@
-time ./PA1 +RTS -N1 
-time ./PA2 +RTS -N2
-time ./PA4 +RTS -N4
-time ./PA8 +RTS -N8
-time ./PA16 +RTS -N16
--- PAsinit.sh
+++
@@ -1,5 +0,0 @@
-time ./PAinit +RTS -N1 
-time ./PAinit +RTS -N2
-time ./PAinit +RTS -N4
-time ./PAinit +RTS -N8
-time ./PAinit +RTS -N16
